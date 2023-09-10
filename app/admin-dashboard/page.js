660203af75555853d2f0c636ee2fab4516cce960
abuzain432432 <abuzain432432@gmail.com>
2023-09-10 07:23:13 -0700

    new bug fixed

--- app/admin-dashboard/page.js
+++ app/admin-dashboard/page.js
@@ -1,41 +1,34 @@
-'use client';
-import AdminDashboardSidebar from '@/components/admin/AdminDashboardSidebar';
-import DashboardAdminAdsRequest from '@/components/DashboardAdminAdsRequest';
-import { useSelector } from 'react-redux';
-import Loading from '@/components/Loading';
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
-import DashboardAdminUsers from '@/components/DashboardAdminUsers';
-import { useContext } from 'react';
-import { AppCtx } from '@/app-context/AppContext';
+"use client";
+import AdminDashboardSidebar from "@/components/admin/AdminDashboardSidebar";
+import DashboardAdminAdsRequest from "@/components/DashboardAdminAdsRequest";
+import { useSelector } from "react-redux";
+import Loading from "@/components/Loading";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import DashboardAdminUsers from "@/components/DashboardAdminUsers";
+import { useContext } from "react";
+import { AppCtx } from "@/app-context/AppContext";
 function Page() {
-  const user = useSelector(state => state.authReducer);
+  const user = useSelector((state) => state.authReducer);
   const router = useRouter();
   const { appLoading } = useContext(AppCtx);
   useEffect(() => {
-    console.log(appLoading);
-    if (
-      !appLoading &&
-      (!user?.token ||
-        !(user?.role === 'admin') ||
-        !(user?.role === 'guide'))
-    ) {
-      router.replace('/');
+    if (!appLoading && (!user?.token || user?.role === "user")) {
+      router.replace("/");
     }
   }, [user?.role, user?.token, appLoading]);
-
-  const [activeTab, setActiveTab] = useState('ads-requests');
-  const handleActiveTab = tab => {
+  const [activeTab, setActiveTab] = useState("ads-requests");
+  const handleActiveTab = (tab) => {
     setActiveTab(tab);
   };
   let content = <DashboardAdminAdsRequest />;
 
-  if (activeTab == 'users') {
+  if (activeTab == "users") {
     content = <DashboardAdminUsers />;
   }
 
   return (
-    <section className='min-h-[100vh] md:flex'>
+    <section className="min-h-[100vh] md:flex">
       {appLoading ? (
         <Loading />
       ) : (
